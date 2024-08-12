49fced92f0d8fe6be3255d5c46698e26ed149449
pnhitd <trandophuongnhi111@gmail.com>
2024-08-12 21:24:45 +0700

    update sample data

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { posts, users } from './data/index.js';
 
 // Configurations
 const __filename = fileURLToPath(import.meta.url);
@@ -59,4 +62,8 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => {
         console.log(`Server is running on port: ${PORT}`);
     })
+
+    // Insert data one time
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
